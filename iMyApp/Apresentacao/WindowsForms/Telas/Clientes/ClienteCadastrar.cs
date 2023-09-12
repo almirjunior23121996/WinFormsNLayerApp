87b44c778255495de760d6e8faaa6678a6a7626f
almirjunior23121996 <128191133+almirjunior23121996@users.noreply.github.com>
2023-09-12 20:19:32 -0300

    Add files via upload

---
+++ iMyApp/Apresentacao/WindowsForms/Telas/Clientes/ClienteCadastrar.cs
@@ -0,0 +1,26 @@
+﻿using Negocio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms.Telas.Clientes
+{
+    public partial class ClienteCadastrar : Form
+    {
+        //Construtor
+        public ClienteCadastrar()
+        {
+            InitializeComponent();
+
+            var cliente = new Cliente();
+
+            var colaborador = new Colaborador();
+        }
+    }
+}
