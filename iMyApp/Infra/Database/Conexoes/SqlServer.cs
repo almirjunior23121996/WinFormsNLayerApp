2397e16739fbeba9ece28d1d91511a715f8a9851
Almir <almir.g.junior@aluno.senai.br>
2023-09-18 21:57:40 -0300

    fazer a implementação de colaborador.

--- iMyApp/Infra/Database/Conexoes/SqlServer.cs
+++ iMyApp/Infra/Database/Conexoes/SqlServer.cs
@@ -6,8 +6,8 @@
         internal static string StrConexao()
         {
 
-            return @"Data Source=CEPEAS21104027\SQLEXPRESS;Initial Catalog=iMyApp;User ID=sa;Password=sql2022";
-            //return "Server=localhost;Database=iMyApp;User Id=sa;Password=sql2022;Trusted_Connection=False; TrustServerCertificate=True;";
+            return @"Data Source=.\SQLEXPRESS;Initial Catalog=DadosCadastraisDoFuncionario;User ID=sa;Password=sql2022";
+            //return "Server =localhost;Database=iMyApp;User Id=sa;Password=sql2022;Trusted_Connection=False; TrustServerCertificate=True;";
         }
     }
 }
